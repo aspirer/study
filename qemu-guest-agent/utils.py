2e2e99f208de5e3b88e94898adada934f35fe6dc
Wangpan <hzwangpan@corp.netease.com>
2013-08-12 19:33:27 +0800

    update qga based cloud vm monitor

---
+++ qemu-guest-agent/utils.py
@@ -0,0 +1,114 @@
+﻿
+import json
+import os
+from xml.etree import ElementTree as ET
+
+from libvirt_qemu import libvirt
+
+
+instances_path = "/var/lib/nova/instances/"
+
+
+
+def is_active(domain):
+    try:
+        return domain.isActive()
+    except libvirt.libvirtError:
+        return False
+
+
+def get_domain_name(domain):
+    try:
+        return domain.name()
+    except libvirt.libvirtError:
+        return None
+
+
+def get_domain_uuid(domain):
+    try:
+        return domain.UUIDString()
+    except libvirt.libvirtError:
+        return None
+
+
+def get_instance_dir(domain):
+    global instances_path
+    try:
+        instance_dir = instances_path + get_domain_name(domain)
+        if not os.path.exists(instance_dir):
+            instance_dir = instances_path + get_domain_uuid(domain)
+
+        if not os.path.exists(instance_dir):
+            return None
+        else:
+            return instance_dir
+    except TypeError:
+        return None
+
+
+def get_info_file_dict(domain, project_id):
+    try:
+        info_file = get_instance_dir(domain) + "/info"
+        with open(info_file, 'r') as f:
+            info_dict = json.loads(f.read())
+
+        # check info file
+        service = info_dict['service']
+        ori_user = info_dict.get('ori_user')
+        aggregation_items = info_dict.get('aggregation_items')
+        resource_type = info_dict['resource_type']
+        if not ori_user and service == 'openstack':
+            info_dict['ori_user'] = project_id
+        if not aggregation_items:
+            info_dict['aggregation_items'] = {}
+        if service == 'openstack' and resource_type != 'openstack':
+            info_dict['resource_type'] = 'openstack'
+
+        return info_dict
+    except (IOError, TypeError, KeyError):
+        return None
+
+
+def get_monitor_setting_root(domain):
+    try:
+        monitor_setting_file = get_instance_dir(domain) + "/monitor_setting.xml"
+        return ET.parse(monitor_setting_file)
+    except (ET.ParseError, IOError, TypeError):
+        return None
+
+
+def get_identify_id(info_file_dict, uuid):
+    if info_file_dict['service'] in ('openstack', 'NVS'):
+        identify_id = uuid
+    else:
+        identify_id = info_file_dict['resource_id']
+
+    return identify_id
+
+
+def get_monitor_metrics(info_file_dict, monitor_setting_root):
+    service = info_file_dict['service']
+    resource_type = info_file_dict['resource_type']
+    setting_services = monitor_setting_root.findall('service')
+    metrics = []
+    for s in setting_services:
+        if (service == s.attrib.get('name') and
+                resource_type == s.attrib.get('resource_type')):
+            metrics = s.findall('metric')
+
+    return metrics
+
+
+def get_aggregation_dimensions(metric, ag_items):
+    ags = metric.findall('aggregation')
+    ag_dims = ''
+    for ag in ags:
+        ag_name = ag.attrib.get('name')
+        if ag_name in ag_items:
+            if ag_dims != '':
+                ag_dims += ','
+            ag_dims += (ag_name + '=' + ag_items[ag_name].encode('utf-8'))
+
+    return ag_dims
+
+
